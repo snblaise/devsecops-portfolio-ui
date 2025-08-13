8ecdb0bc726ae1d7889d8e9855c29518365bd1e7
Blaise Ngwa <blaisengwa@Mac.attlocal.net>
2025-08-13 19:12:51 -0400

    updating ui

--- next.config.js
+++ next.config.js
@@ -25,15 +25,7 @@ const config = withPWA({
     unoptimized: true,
   },
 
-  /**
-   * If you are using `appDir` then you must comment the below `i18n` config out.
-   *
-   * @see https://github.com/vercel/next.js/issues/41980
-   */
-  i18n: {
-    locales: ["en"],
-    defaultLocale: "en",
-  },
+
 });
 
 export default config;
